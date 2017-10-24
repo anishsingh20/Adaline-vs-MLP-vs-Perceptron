d0678bfdff8b82e9cec4b9afbdfc95c9464d1bc6
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-24 22:46:57 +0530

    making a MLP

--- adalineVsMLP.R
+++ adalineVsMLP.R
@@ -1,1 +1,15 @@
 #Adaline vs MLP on Iris dataset
+
+
+#MAKING A MLP
+
+require(keras)
+
+#separating test an training data
+head(iris)
+nrow(iris)#150 rows
+dim(iris)
+
+iris<-as.matrix(iris)
+iris.trainX<-iris[,1:4]
+iris.trainY<-iris[,5]

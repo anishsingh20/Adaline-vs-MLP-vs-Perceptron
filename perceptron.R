c35a11da9716f9395ca9a8040a0e9babd0ded3c4
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-25 22:16:47 +0530

    initialized weights and error vector

--- perceptron.R
+++ perceptron.R
@@ -5,6 +5,11 @@
 #function takes input as inputs,target vector, learning rate and epochs
 perceptronNet <- function(x,y,lr,epochs) 
 {
+  #initializing weights vector
+  weights<-rep(0,dim(x)[2]+1) # a vector of length 5.As we have 4 variables
+  
+  #initialize empty error vector for each epoch
+  errors<-rep(0,epochs)
   
   
 }

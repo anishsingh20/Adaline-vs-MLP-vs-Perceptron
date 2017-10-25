8bfe7ccf0fd526365f1c0e069571a1f4be25bcf4
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-25 22:30:48 +0530

    implemented the logic behind preceptron rule algorithm

--- perceptron.R
+++ perceptron.R
@@ -6,8 +6,8 @@
 perceptronNet <- function(x,y,lr,epochs) 
 
 { 
-  #initializing weights vector
-  weights<-rep(0,dim(x)[2]+1) # a vector of length 5.As we have 4 variables
+  #initializing weights vector with dimension equal to the number of input variables
+  weight<-rep(0,dim(x)[2]+1) # a vector of length 5.As we have 4 variables
   
   #initialize empty error vector for each epoch
   errors<-rep(0,epochs)
@@ -20,6 +20,15 @@ perceptronNet <- function(x,y,lr,epochs)
         for(j in 1:length(y))
           
         {
+          #sum of product of weights and inputs added with bias
+          z=sum( weight[2:length(weight)]*as.numeric(x[j,] ) ) + weight[1]#bias
+          
+          #applying activation function(unit step function)
+          if(z>0){
+            y_out<- 1
+          } else {
+            y_out <- -1
+          }
           
         }
     

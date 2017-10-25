a90afaa3a9bdbe0bc183a5ebc7de2f053081bf74
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-25 22:12:01 +0530

    making a function for implementing perceptron algorithm

--- perceptron.R
+++ perceptron.R
@@ -1,2 +1,10 @@
 #Perceptron rule algorithm to do multiclass classification on Iris dataset
 
+#making a function for implementing perceptron rule algorithm
+
+#function takes input as inputs,target vector, learning rate and epochs
+perceptronNet <- function(x,y,lr,epochs) 
+{
+  
+  
+}

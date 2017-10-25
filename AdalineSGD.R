a3b96c9f855daadb52e2b5617c2e73b2d5f40096
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-25 12:48:29 +0530

    testing the adaline network

--- AdalineSGD.R
+++ AdalineSGD.R
@@ -5,9 +5,21 @@
 require(nnet)
 ?nnet
 
-#training data for Adaline network
+#training data for multiple Adaline network-MADALINE
 trainAda<-cbind(iris.trainX,iris.trainY)
 
 adaline <- nnet(iris.trainY ~ ., data = trainAda,
                 size = 8, rang = 0.1,
                decay = 5e-4, maxit = 200)
+
+#picking out the weights
+adaline$wts
+adaline$nunits#127 units-neurons
+adaline$nconn
+adaline$conn
+adaline$softmax
+adaline$convergence
+adaline$fitted.values
+adaline$residuals
+#predicting on test set
+table(ird$species[-samp], predict(ir.nn2, ird[-samp,], type = "class"))

b3cfbb94fdd526e64986e94537cb5303e8cdcbf1
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-25 12:40:47 +0530

    Trained the adaline network

--- AdalineSGD.R
+++ AdalineSGD.R
@@ -4,3 +4,10 @@
 
 require(nnet)
 ?nnet
+
+#training data for Adaline network
+trainAda<-cbind(iris.trainX,iris.trainY)
+
+adaline <- nnet(iris.trainY ~ ., data = trainAda,
+                size = 8, rang = 0.1,
+               decay = 5e-4, maxit = 200)
